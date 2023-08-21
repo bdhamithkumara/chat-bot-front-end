a3cb20cc3aa46b61f1c48b19ddaa6c25efe664ad
dhamithdev <126062427+dhamithdev@users.noreply.github.com>
2023-08-22 01:26:02 +0530

    setup navbar

---
+++ src/components/Mainwindow.js
@@ -0,0 +1,27 @@
+"use client"
+
+
+
+
+const Mainwindow = () => {
+
+
+
+
+  return (
+    <>
+   
+
+
+
+      <div  className="w-full h-18  fixed flex justify-between">
+      <div  className=" text-3xl font-bold p-5 font-sans">DKBot</div>
+    </div>
+<br/>
+
+
+    </>
+  )
+}
+
+export default Mainwindow
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useTheme } from 'next-themes'
+import { FaSun, FaRegMoon } from "react-icons/fa";
+import {BsFillVolumeUpFill, BsFillVolumeMuteFill} from 'react-icons/bs'
+
+const Navbar = () => {
+
+    const { theme, setTheme } = useTheme()
+
+
+    return (
+        <>
+            <nav className=" fixed w-full z-20 top-0 left-0 border-b border-gray-200 ">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap ">Flowbite</span>
+                    <div className='flex'>
+                        <button className="block border-2 border-black  rounded " onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+                            {theme === "dark" ? <FaSun className='m-2' /> : <FaRegMoon className='m-2' />}
+                        </button>
+                        <button className="block border-2 border-black  rounded " >
+                        {theme === "dark" ? <BsFillVolumeUpFill className='m-2' /> : <BsFillVolumeMuteFill className='m-2' />}
+                    </button>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
